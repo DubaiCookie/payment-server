576c4909a58023aa035736e55fa9ea6caa9989ff
sisoin <sokk159dps@gmail.com>
2026-04-28 08:54:15 +0900

    feat: 만료 주문 자동 처리 스케줄러 추가
    - PENDING 상태이면서 expiredAt이 지난 주문을 1분 간격으로 EXPIRED로 전환
    - OrderExpirationScheduler 신규 구현
    - @EnableScheduling 활성화

--- src/main/java/com/payserver/PayServerApplication.java
+++ src/main/java/com/payserver/PayServerApplication.java
@@ -2,8 +2,11 @@ package com.payserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+// @EnableScheduling: @Scheduled 어노테이션이 붙은 스케줄러 빈을 활성화한다
 @SpringBootApplication
+@EnableScheduling
 public class PayServerApplication {
 
     public static void main(String[] args) {
